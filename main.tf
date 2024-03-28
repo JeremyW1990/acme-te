ec4eb79434c1faf1ccfbcb66a835f79987d9b852
Jeremy Wang <shijie.wang1990@gmail.com>
2024-03-27 19:05:27 -0500

    Rove the vpc module to a seperate repo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source = "https://github.com/JeremyW1990/acme-module-vpc.git?ref=v1.0.0"
 
   cidr_block        = "10.0.0.0/16"
   public_subnets    = ["10.0.1.0/24", "10.0.2.0/24"]
