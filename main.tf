88db1b9465a4a9f4f14c10bbb55d839e0e67bdae
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-15 10:49:11 -0500

    Delete the ec2 module for testing

--- main.tf
+++ main.tf
@@ -7,20 +7,3 @@ module "vpc" {
   availability_zones = ["us-east-1a", "us-east-1b"]
   vpc_name          = "acme-vpc"
 }
-
-module "acme_web_ec2_asg_alb" {
-  source = "github.com/JeremyW1990/acme-module-ec2_asg_alb?ref=v1.0.0"
-
-  environment       = var.environment
-  image_id          = "ami-0bd01824d64912730" # Example AMI ID; replace with a valid one
-  instance_type     = "t2.micro"
-  security_group_id = module.vpc.security_group_id
-  subnet_ids        = module.vpc.public_subnet_ids
-  vpc_id            = module.vpc.vpc_id
-  user_data = templatefile("./user_data.tpl", { environment = var.environment })
-
-  
-  asg_min_size         = 1
-  asg_max_size         = 3
-  asg_desired_capacity = 2
-}
