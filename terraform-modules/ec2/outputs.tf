684d2f98a409b697cb2f6587ea4f2baa7044048e
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-19 00:01:48 -0500

    reset the repo

--- terraform-modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "asg_name" {
-  description = "The name of the autoscaling group"
-  value       = aws_autoscaling_group.asg.id
-}
-
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = aws_lb.alb.dns_name
-}
