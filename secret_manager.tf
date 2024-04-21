671705540eaced6a2dfcf341efe901df866e135e
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-21 15:13:27 -0500

    wip

--- secret_manager.tf
+++ secret_manager.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "secret_manager_from_AI" {
-  name       = "secret_manager_from_AI"
+  name     = "secret_manager_from_AI"
+
   tags = {
     owner = "jeremy"
   }
