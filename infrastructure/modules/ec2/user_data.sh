448a22b3d48179ec22ee9a336dc34e3f9b75fa3f
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-23 17:45:55 -0500

    clear repo

--- infrastructure/modules/ec2/user_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A placeholder script for user data, used to bootstrap instances.
-# Replace the content of this script with the actual steps needed to bootstrap your application.
-echo "Hello, World!" > /var/www/html/index.html
-sudo service httpd start
