dfd2514300bab28119fd5344114a041cfd63aa6a
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-23 17:53:49 -0500

    wip

---
+++ ssm.tf
@@ -0,0 +1,8 @@
+resource "aws_ssm_parameter" "example_parameter" {
+  name  = "/config/service/example-parameter"
+  type  = "String"
+  value = "ExampleValue"
+  tags = {
+    Environment = "development"
+  }
+}
