ad5f0463298ac1697d5290c46ef41e7cbf164feb
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-27 15:44:09 -0500

    delete

--- ssm.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ssm_parameter" "example_parameter" {
-  name  = "/config/service/example-parameter"
-  type  = "String"
-  value = "ExampleValue"
-  tags = {
-    Environment = "development"
-  }
-}
-
