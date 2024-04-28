74818c084e4302f4a6a0984b61fad872fc3631a8
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-28 09:48:38 -0500

    reset repo

--- modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "The DNS name of the Application Load Balancer"
-  value       = aws_alb.this.dns_name
-}
-
-output "asg_name" {
-  description = "The name of the Autoscaling Group"
-  value       = aws_autoscaling_group.this.name
-}
