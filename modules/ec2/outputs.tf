9aae3a842178dc199f7eaf51b9d3b9e342058a6f
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-30 18:31:23 -0500

    reset

--- modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  value       = aws_alb.main.dns_name
-  description = "The DNS name of the application load balancer"
-}
-
-output "app_security_group_id" {
-  value       = aws_security_group.ec2_sg.id
-  description = "The security group ID for the application EC2 instances"
-}
