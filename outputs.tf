aebe10a3697f2bbc595480a7434af4a208c6f0d4
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-30 16:27:00 -0500

    reset

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "alb_dns_name" {
-  description = "The DNS name for the ALB to access the application"
-  value       = module.my_ec2.alb_dns_name
-}
--- userdata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Install a basic web server and write the homepage
-yum -y update
-yum -y install httpd
-systemctl start httpd
-systemctl enable httpd
-echo "Auto Deployment from Cloud-AI" > /var/www/html/index.html
