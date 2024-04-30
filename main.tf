103c721e45548eb1aa9347cb4cf4c1e5c541ad2c
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-30 18:29:38 -0500

    wip

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name" # Ensure this bucket name is unique globally.
-  acl    = "private" # This sets the bucket to be private.
+module "vpc" {
+  source = "../modules/vpc"
 
-  tags = {
-    Name        = "My S3 Bucket"
-    Environment = "Dev"
-  }
+  vpc_cidr              = "10.0.0.0/16"
+  public_subnets_cidr   = ["10.0.1.0/24", "10.0.2.0/24"]
+  availability_zones    = ["us-east-1a", "us-east-1b"]
+}
+
+module "ec2" {
+  source = "../modules/ec2"
+
+  vpc_id           = module.vpc.vpc_id
+  subnet_ids       = module.vpc.public_subnets
+  ami_id           = "ami-0b0ea68c435eb488d"
+  instance_type    = "t2.micro"
+  min_size         = 1
+  max_size         = 2
+  desired_capacity = 1
+}
+
+output "application_url" {
+  value = "http://${module.ec2.alb_dns_name}"
+  description = "URL to access the deployed application"
 }
