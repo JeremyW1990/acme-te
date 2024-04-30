371d24611c5c12b90bb36c41f1f45dea905da710
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-30 16:09:30 -0500

    reset

--- module/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-    description = "The DNS name of the ALB"
-    value       = aws_alb.app.dns_name
-}
-
-output "asg_name" {
-    description = "The name of the Auto Scaling Group"
-    value       = aws_autoscaling_group.app.name
-}
