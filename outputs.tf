ea3d48b55a19c1e1d3b6d25a9d0f1f81ade089a3
Jeremy Wang <shijie.wang1990@gmail.com>
2024-04-30 17:03:09 -0500

    reset

--- outputs.tf
+++
@@ -1,2 +0,0 @@
-# This file already has outputs in main.tf, which is preferred in root configurations.
-# You can also define additional global outputs here if needed.
--- variables.tf
+++
@@ -1,2 +0,0 @@
-# This file can remain empty as we are hardcoding the values in the main.tf
-# If you want to use variables, you can define them here and pass them to the modules in main.tf
