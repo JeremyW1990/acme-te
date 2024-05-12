b52d840256fdd15232081e6625158e2cb68e1c35
Jeremy Wang <shijie.wang1990@gmail.com>
2024-05-12 18:22:14 -0500

    update variable to lb_target_group_arn. test again

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -80,6 +80,8 @@ resource "aws_alb_listener" "alb_listener" {
 }
 
 resource "aws_autoscaling_attachment" "asg_attachment" {
-  autoscaling_group_name   = aws_autoscaling_group.asg.id
-  alb_target_group_arn     = aws_alb_target_group.alb_target_group.arn
+  autoscaling_group_name = aws_autoscaling_group.asg.id
+  lb_target_group_arn    = aws_alb_target_group.alb_target_group.arn
 }
+
+# ... (other configuration remains unchanged)
