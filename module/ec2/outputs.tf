166fe8cd7cdc2b0769091be7388e777d07e63701
Jeremy Wang <shijie.wang1990@gmail.com>
2024-05-12 20:03:33 -0500

    destroy

--- module/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_dns_name" {
-  description = "DNS name of the application load balancer"
-  value       = aws_alb.application_load_balancer.dns_name
-}
-
-output "asg_name" {
-  description = "The autoscaling group name"
-  value       = aws_autoscaling_group.asg.name
-}
